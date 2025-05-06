39689a2e6fb0e83838b38d33735703f4491b4540
nvhanhzz <nvhanh263@gmail.com>
2025-05-06 11:55:17 +0700

    fix(ui): fix border top for footer, and some component

--- src/client/pages/WatchMovie/Recommended/index.tsx
+++ src/client/pages/WatchMovie/Recommended/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import "./Recommended.scss";
 
 const Recommended: React.FC = () => {
     return (
-        <>
+        <div className={ "recommended"}>
             Recommended
-        </>
+        </div>
     )
 };
 
